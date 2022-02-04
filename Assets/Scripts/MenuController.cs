027e936fadc0a75a1edd50abac953bd6eb6e0e61
JuniorJean02 <fatjun_02@yahoo.fr>
2022-02-03 19:06:02 -0500

    test

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    //[SerializeField] private GameObject confirmationPrompt = null;
+    [SerializeField] private float defaulVolume = 1.0f;
+
+    [Header("Graphics Setting")]
+    private int _qualityLevel;
+    private bool _isFullScreen;
+
     [Header("Levels To load")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGame = null;
+
+    [Header("Resolution Dropdown")]
+    public TMP_Dropdown resolutionDropdown;
+    private  Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutionDropdown = GetComponent<TMP_Dropdown>();
+        resolutions = Screen.resolutions;
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length ; i++)
+            {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height && resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+    }
+
+    public void SetResolution()
+    {
+        Resolution resolution = resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode, resolution.refreshRate);
+    }
 
     public void NewGameDialogYes()
     {
@@ -33,5 +74,57 @@ public class MenuController : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        //StartCoroutine(ConfirmationBox());
+        //show Promt
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        _isFullScreen = isFullScreen;
+    }
+
+    public void SetQuality(int isQuality)
+    {
+        _qualityLevel = isQuality;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullScreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+    }
+
+    public void ResetButton(string Menutype)
+    {
+        if(Menutype == "Audio")
+        {
+            AudioListener.volume = defaulVolume;
+            volumeSlider.value = defaulVolume;
+            volumeTextValue.text = defaulVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
     
+    //public IEnumerator ConfirmationBox()
+    //{
+    //    confirmationPrompt.SetActive(true);
+    //    yield return new WaitForSeconds(2);
+    //    confirmationPrompt.SetActive(false);
+    //}
+        
 }
